7218af964d174e6322398349a54eb06bb5bfb819
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-08-24 21:19:20 +0200

    video 08 object and pick card

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
@@ -8,12 +9,33 @@ import { Component } from '@angular/core';
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
-
+export class GameComponent implements OnInit{
   pickCardAnimation:boolean =  false;
+  currentCard: string = '';
+  game!: Game;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game)
+  }
 
   takeCard() {
-    this.pickCardAnimation = true;
+    if(!this.pickCardAnimation){
+      this.currentCard = this.game.stack.pop() || '';
+      console.log(this.currentCard);
+      this.pickCardAnimation = true;
+    }
+
+    setTimeout(() => {
+      this.pickCardAnimation = false
+    }, 1500)
   }
 
 }
