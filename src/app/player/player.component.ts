bf41a346cf5572df8104c136aa267aeacd03bbd8
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-08-24 22:59:37 +0200

    added player icons

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
 
 @Component({
   selector: 'app-player',
@@ -8,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './player.component.scss'
 })
 export class PlayerComponent {
+
+  @Input() name: any;
 
 }
