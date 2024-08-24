8ad8352f7d77d4b578ef48c9c3fdf935d8c5a3bd
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-08-24 13:21:43 +0200

    link to game.component and style of main div

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { GameComponent } from '../game/game.component';
 
 @Component({
   selector: 'app-start-screen',
   standalone: true,
-  imports: [],
+  imports: [GameComponent],
   templateUrl: './start-screen.component.html',
   styleUrl: './start-screen.component.scss'
 })
 export class StartScreenComponent {
 
+  constructor(private router: Router) {
+   
+   
+  }
+
+  newGame() {
+    // Start game
+    this.router.navigateByUrl('/game');
+  }
 }
+
+
