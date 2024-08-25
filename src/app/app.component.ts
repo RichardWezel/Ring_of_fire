d641737d061b532df92cdd6ac4de23ad588777c8
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-08-25 09:53:24 +0200

    installed material design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StartScreenComponent } from "./start-screen/start-screen.component";
+import { MatButtonModule, MatIconButton } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, StartScreenComponent],
+  imports: [RouterOutlet, StartScreenComponent, MatButtonModule, MatIconButton, MatIconModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
