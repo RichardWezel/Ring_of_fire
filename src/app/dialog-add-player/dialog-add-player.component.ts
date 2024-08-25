9c8ebf4332a782d9b4bb8803e9900eaa3ebe0a34
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-08-25 16:32:28 +0200

    finish

--- src/app/dialog-add-player/dialog-add-player.component.ts
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {
   MatDialogActions,
   MatDialogClose,
   MatDialogContent,
+  MatDialogModule,
   MatDialogTitle,
+  MatDialogRef,
 } from '@angular/material/dialog';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
@@ -13,15 +15,19 @@ import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-dialog-add-player',
   standalone: true,
-  imports: [MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle, FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle, FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule,  MatDialogModule],
   templateUrl: './dialog-add-player.component.html',
   styleUrl: './dialog-add-player.component.scss'
 })
 export class DialogAddPlayerComponent {
   name: string = '';
+  readonly dialogRef = inject(MatDialogRef<DialogAddPlayerComponent>);
+
+
+
 
   onNoClick() {
-
+    this.dialogRef.close();
   }
 
 }
