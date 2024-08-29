e4a924265465337496ac3aecac69a3a8e0cb8b3e
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-08-29 21:46:42 +0200

    debugging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StartScreenComponent } from "./start-screen/start-screen.component";
 import { MatButtonModule, MatIconButton } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-
+import { BehaviorSubject, interval, throttle } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +12,22 @@ import { MatIconModule } from '@angular/material/icon';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ringoffire';
+
+  timer = new BehaviorSubject<number>(0);
+
+  ngOnInit() {
+    this.timer
+    .pipe(throttle(val => interval(2000)))
+    .subscribe((timepassed) => {
+      console.log(timepassed);
+
+    });
+
+   setInterval(() => {
+    let newValue = this.timer.value + 1000;
+    this.timer.next(newValue)
+   }, 100)
+  }
 }
